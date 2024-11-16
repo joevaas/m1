01f8f082f74fe2f750a3ef83bc84fd27342cdb67
joevaas <133934323+joevaas@users.noreply.github.com>
2024-11-16 13:41:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -154,7 +154,7 @@ async def not_joined(client: Client, message: Message):
         pass
 
     await message.reply_photo(
-    photo=FORCE_PIC, 
+    photo=F_PIC, 
     caption=FORCE_MSG.format(
         first=message.from_user.first_name,
         last=message.from_user.last_name,
