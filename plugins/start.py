49e031c20a9cfabbf4d751ae8934c71ecdbc4d37
joevaas <133934323+joevaas@users.noreply.github.com>
2024-11-16 13:34:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -114,17 +114,16 @@ async def start_command(client: Client, message: Message):
         ]
     ]
        )
-        await message.reply_text(
-            text = START_MSG.format(
+        await message.reply_photo(
+            photo = START_PIC,
+            caption = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
                 mention = message.from_user.mention,
                 id = message.from_user.id
             ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
+            reply_markup = reply_markup
         )
         return
 
@@ -154,18 +153,6 @@ async def not_joined(client: Client, message: Message):
     except IndexError:
         pass
 
-    await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-        reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
-    )
     await message.reply_photo(
     photo=FORCE_PIC, 
     caption=FORCE_MSG.format(
