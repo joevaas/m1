12e036b97f91667067cd2103b4e0e772e902ac69
joevaas <133934323+joevaas@users.noreply.github.com>
2025-01-25 21:29:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7427035170:AAF7cOQicjzC7uUmbVpxz6YXT3Mxr0hlRKI")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7427035170:AAHjUbj--QvG5vOMIZx98cHDmjaAcGfQjlw")
 API_ID = int(os.environ.get("API_ID", "25797857"))
 API_HASH = os.environ.get("API_HASH", "77717127ece56fac64ebea6250db8bb7")
 
