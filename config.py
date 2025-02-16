4d1019667c09eaf08a35005bba40429633a0ff25
joevaas <133934323+joevaas@users.noreply.github.com>
2025-02-16 10:34:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ DB_NAME = os.environ.get("DB_NAME", "Naruto_TAF")
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002093054178"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002005092018"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002412330704"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002433169699"))
 
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
